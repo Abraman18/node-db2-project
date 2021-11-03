fa0ba24bf07a06a0d9bb6fd90888db8ace9c8a85
Abraman18 <abraman18@gmail.com>
2021-11-02 18:44:20 -0700

    inititla get response is working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,13 @@
-// DO YOUR MAGIC
+// const { json } = require('express')
+const router = require('express').Router()
+const Cars = require('./cars-model')
+
+router.get('/', async (req, res, next) => {
+	await Cars.getAll(req.query)
+		.then(cars => {
+		res.status(200).json(cars)
+		})
+	.catch(next)
+})
+
+module.exports = router;
